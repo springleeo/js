58737db39bf42d240d417c2c4f69f3044f196068
22413 <liqiang6@xiamenair.com>
2017-05-03 08:05:33 +0800

    增加弹窗

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -3,3 +3,9 @@ function showPic(whichpic) {
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
 }
+
+function countBodyChildren() {
+    var bodyElement = document.getElementsByTagName("body")[0];
+    alert(bodyElement.childNodes.length)
+}
+window.onload = countBodyChildren
